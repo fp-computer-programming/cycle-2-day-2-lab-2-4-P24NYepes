acdad53fa30bd57377bacd45ce53c7d309ecda20
Nicholas Yepes <p24nyepes@fairfieldprep.org>
2023-09-21 12:39:04 -0400

    lab submission

---
+++ lab_2-4.py
@@ -0,0 +1,16 @@
+a=1
+b=2
+c=3
+x=2
+y=5
+
+first= (3+(4*x))/5
+seccond= 10*(y-5)*(a+b+c)**2/x
+third= (4/(x**2))+((9+x)/y)
+
+part1 = first - seccond
+part3= third*9
+part2 = part1 +part3
+print(part2)
+#(3+x/, 5)-(10(y-5)(a+b+c)
+# ^2)+9((4/x^2)+(9+x/y))
